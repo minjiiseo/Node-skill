af4bfe8072053a36335aa2ab379aa3d4eb1d7fce
minjiiseo <167045049+minjiiseo@users.noreply.github.com>
2024-05-28 21:23:03 +0900

    로그인 기능 개발

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routers/users.router.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { prisma } from '../utils/prisma.util.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -45,6 +46,31 @@ router.post('/sign-up', async (req, res) => {
     createdAt: user.createdAt,
     updatedAt: user.updatedAt,
   });
+});
+
+router.post('/sign-in', async (req, res) => {
+  const { email, password } = req.body;
+
+  // 유효성 검증
+  if (!email) return res.status(400).send('이메일을 입력해 주세요.');
+  if (!password) return res.status(400).send('비밀번호를 입력해 주세요.');
+
+  const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+  if (!emailRegex.test(email)) return res.status(400).send('이메일 형식이 올바르지 않습니다.');
+
+  // 사용자 확인
+  const user = await prisma.users.findUnique({ where: { email } });
+  if (!user) return res.status(400).send('인증 정보가 유효하지 않습니다.');
+
+  // 비밀번호 확인
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) return res.status(400).send('인증 정보가 유효하지 않습니다.');
+
+  // AccessToken 생성
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '12h' });
+
+  // AccessToken 반환
+  res.status(200).json({ accessToken: token });
 });
 
 export default router;
